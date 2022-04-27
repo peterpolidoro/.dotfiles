cf7a2215ee6be0e53b618f045d902197a4ee142a
Peter Polidoro <peter@polidoro.io>
2022-04-27 11:05:18 -0400

    Update guix channels to use substitutes

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -1,3 +1,9 @@
+(use-modules (guix ci))
+
+(list (channel-with-substitutes-available
+       %default-guix-channel
+       "https://ci.guix.gnu.org"))
+
 (cons* (channel
         (name 'guix-janelia)
         (url "https://github.com/guix-janelia/guix-janelia.git")
