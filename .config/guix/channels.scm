b2e0a7fa9b7e9bf91eb8150fe39666b43edd8bf2
Peter Polidoro <peter@polidoro.io>
2023-11-01 14:48:04 -0400

    Remove guix-janelia from default channels

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -3,9 +3,3 @@
 (list (channel-with-substitutes-available
        %default-guix-channel
        "https://ci.guix.gnu.org"))
-
-(cons* (channel
-        (name 'guix-janelia)
-        (url "https://github.com/guix-janelia/guix-janelia.git")
-        (branch "main"))
-       %default-channels)
