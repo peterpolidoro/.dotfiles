6f00e779730797e4b67adfed033e67df34c4943f
Peter Polidoro <peter@polidoro.io>
2025-11-21 13:26:00 -0500

    Update custom.el

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -3,3 +3,46 @@
 ;; It is intentionally kept out of the reusable package.
 
 ;; Nothing here by default.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("1ad12cda71588cc82e74f1cabeed99705c6a60d23ee1bb355c293ba9c000d4ac"
+     "328c439b1edb3422e7afdb0b603eac6018475f536fbabe5fd858030607a7de83" default))
+ '(org-fold-core-style 'overlays)
+ '(safe-local-variable-values
+   '((eval modify-syntax-entry 43 "'") (eval modify-syntax-entry 36 "'")
+     (eval modify-syntax-entry 126 "'") (lisp-fill-paragraphs-as-doc-string nil)
+     (geiser-insert-actual-lambda) (geiser-repl-per-project-p . t)
+     (eval with-eval-after-load 'yasnippet
+           (let
+               ((guix-yasnippets
+                 (expand-file-name "etc/snippets/yas"
+                                   (locate-dominating-file default-directory
+                                                           ".dir-locals.el"))))
+             (unless (member guix-yasnippets yas-snippet-dirs)
+               (add-to-list 'yas-snippet-dirs guix-yasnippets) (yas-reload-all))))
+     (eval with-eval-after-load 'tempel
+           (if (stringp tempel-path) (setq tempel-path (list tempel-path)))
+           (let
+               ((guix-tempel-snippets
+                 (concat
+                  (expand-file-name "etc/snippets/tempel"
+                                    (locate-dominating-file default-directory
+                                                            ".dir-locals.el"))
+                  "/*.eld")))
+             (unless (member guix-tempel-snippets tempel-path)
+               (add-to-list 'tempel-path guix-tempel-snippets))))
+     (eval with-eval-after-load 'git-commit
+           (add-to-list 'git-commit-trailers "Change-Id"))
+     (eval setq-local guix-directory
+           (locate-dominating-file default-directory ".dir-locals.el"))
+     (eval add-to-list 'completion-ignored-extensions ".go"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
