376161900ff531d023803e6e79e266ab3b7ad04b
Peter Polidoro <peter@polidoro.io>
2025-11-24 10:19:04 -0500

    Remove embark-consult

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -670,16 +670,6 @@
                  nil
                  (window-parameters (mode-line-format . none)))))
 
-;; Consult users will also want the embark-consult package.
-(use-package embark-consult
-  :after (embark consult)
-  :demand t ; only necessary if you have the hook below
-  ;; if you want to have consult previews as you move around an
-  ;; auto-updating embark collect buffer
-  :hook
-  (embark-collect-mode . consult-preview-at-point-mode)
-  )
-
 (add-to-list 'completion-ignored-extensions ".go")
 
 (use-package calc
